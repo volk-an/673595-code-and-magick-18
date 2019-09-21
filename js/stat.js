b43b9de52b4143f1b084f2350d2eb3b45896f80f
volk-an <volk-an@mail.ru>
2019-09-21 12:27:31 +0700

    доработка таблички цвета,время

--- js/stat.js
+++ js/stat.js
@@ -1,29 +1,30 @@
+'use strict';
 var cloudParameters = {
-CLOUD_WIDTH: 420,
-CLOUD_HEIGHT: 270,
-CLOUD_X: 100,
-CLOUD_Y: 10,
-SHADDOW_GAP: 10,
-CLOUD_GAP_X: 50,
-CLOUD_GAP_Y: 40
+  WIDTH: 420,
+  HEIGHT: 270,
+  X: 100,
+  Y: 10,
+  SHADDOW_GAP: 10,
+  GAP_X: 50,
+  GAP_Y: 40
 };
 var MESSAGE_GAP_Y = 20;
 var barParameters = {
-BAR_MAX_HEIGHT: 150,
-BAR_WIDTH: 40,
-BAR_Y: 90,
-BAR_GAP: 50
-}
-var NAME_BOTTOM_GAP = 20;
-var renderCloud = function(ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, cloudParameters.CLOUD_WIDTH, cloudParameters.CLOUD_HEIGHT);
+  MAX_HEIGHT: 150,
+  WIDTH: 40,
+  Y: 90,
+  GAP: 50
 };
-//var renderBar = function(ctx, x, y, color) {
+var NAME_BOTTOM_GAP = 20;
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, cloudParameters.WIDTH, cloudParameters.HEIGHT);
+};
+// var renderBar = function(ctx, x, y, color) {
 //  ctx.fillStyle = color;
 //  ctx.fillRect(x, y, BAR_WIDTH, BAR_HEIGHT);
-//};
-/*var getMaxElement = function(arr) {
+// };
+/* var getMaxElement = function(arr) {
   var maxElement = arr[0];
   for (var i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
@@ -32,30 +33,30 @@ var renderCloud = function(ctx, x, y, color) {
   };
     return maxElement;
 };*/
-var getMaxOfArray = function(numArray) {
+var getMaxOfArray = function (numArray) {
   return Math.max.apply(null, numArray);
 };
 
-window.renderStatistics = function(ctx, players, times) {
-  renderCloud(ctx, cloudParameters.CLOUD_X + cloudParameters.SHADDOW_GAP, cloudParameters.CLOUD_Y + cloudParameters.SHADDOW_GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, cloudParameters.CLOUD_X, cloudParameters.CLOUD_Y, '#fff');
-  //текст таблички
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, cloudParameters.X + cloudParameters.SHADDOW_GAP, cloudParameters.Y + cloudParameters.SHADDOW_GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, cloudParameters.X, cloudParameters.Y, '#fff');
+  // текст таблички
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', cloudParameters.CLOUD_X + cloudParameters.CLOUD_GAP_X, cloudParameters.CLOUD_GAP_Y);
-  ctx.fillText('Список результатов:', cloudParameters.CLOUD_X + cloudParameters.CLOUD_GAP_X, cloudParameters.CLOUD_GAP_Y + MESSAGE_GAP_Y);
-  //диаграмма
+  ctx.fillText('Ура вы победили!', cloudParameters.X + cloudParameters.GAP_X, cloudParameters.GAP_Y);
+  ctx.fillText('Список результатов:', cloudParameters.X + cloudParameters.GAP_X, cloudParameters.GAP_Y + MESSAGE_GAP_Y);
+  // диаграмма
 
   for (var i = 0; i < players.length; i++) {
-  var playerX = cloudParameters.CLOUD_X + cloudParameters.CLOUD_GAP_X + (barParameters.BAR_WIDTH + barParameters.BAR_GAP) * i;
-  ctx.fillStyle = '#000';
-  ctx.fillText(players[i], playerX, cloudParameters.CLOUD_HEIGHT + cloudParameters.CLOUD_Y - NAME_BOTTOM_GAP);
-  if(players[i] === 'Вы') {
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';} else {
-    ctx.fillStyle = 'hsl(240, 100%, 50%)'
-  };
-  var maxTime = getMaxOfArray(times);
-  var barHeight = times[i] * barParameters.BAR_MAX_HEIGHT / maxTime;
-  ctx.fillRect(playerX, barParameters.BAR_Y + barParameters.BAR_MAX_HEIGHT - barHeight, barParameters.BAR_WIDTH, barHeight);
-};
+    var coordX = cloudParameters.X + cloudParameters.GAP_X + (barParameters.WIDTH + barParameters.GAP) * i;
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], coordX, cloudParameters.HEIGHT + cloudParameters.Y - NAME_BOTTOM_GAP);
+    ctx.fillStyle = players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + Math.random() * 100 + '%, 50%)';
+    var maxTime = getMaxOfArray(times);
+    var barHeight = times[i] * barParameters.MAX_HEIGHT / maxTime;
+    var coordY = barParameters.Y + barParameters.MAX_HEIGHT - barHeight;
+    ctx.fillRect(coordX, coordY, barParameters.WIDTH, barHeight);
+    ctx.fillStyle = '#000';
+    ctx.fillText(Math.round(times[i]), coordX, coordY - 10);
+  }
 };
