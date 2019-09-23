850c23bbfa41030c51eea49f7a9a8dbbccf1c6ad
volk-an <volk-an@mail.ru>
2019-09-23 22:00:25 +0700

    setup созд. персонажей без функций

--- js/setup.js
+++ js/setup.js
@@ -12,15 +12,36 @@ var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var get_random = function (array) {
   return array[Math.floor((Math.random()*array.length))];
 }
+var wizards = [
+{
+  name: get_random(WIZARD_NAMES) + ' ' + get_random(WIZARD_SURNAMES),
+  coatColor: get_random(COAT_COLORS),
+  eyesColor: get_random(EYES_COLORS)
+},
+{
+  name: get_random(WIZARD_NAMES) + ' ' + get_random(WIZARD_SURNAMES),
+  coatColor: get_random(COAT_COLORS),
+  eyesColor: get_random(EYES_COLORS)
+},
+{
+  name: get_random(WIZARD_NAMES) + ' ' + get_random(WIZARD_SURNAMES),
+  coatColor: get_random(COAT_COLORS),
+  eyesColor: get_random(EYES_COLORS)
+},
+{
+  name: get_random(WIZARD_NAMES) + ' ' + get_random(WIZARD_SURNAMES),
+  coatColor: get_random(COAT_COLORS),
+  eyesColor: get_random(EYES_COLORS)
+}];
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-for (var i = 0; i < 4; i++) {
+for (var i = 0; i < wizards.length; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-  var name = get_random(WIZARD_NAMES) + ' ' + get_random(WIZARD_SURNAMES);
-  var coatColor = get_random(COAT_COLORS);
-  var eyesColor = get_random(EYES_COLORS);
-  wizardElement.querySelector('.setup-similar-label').textContent = name;
-  wizardElement.querySelector('.wizard-coat').style.fill = coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = eyesColor;
+  //var name = get_random(WIZARD_NAMES) + ' ' + get_random(WIZARD_SURNAMES);
+  //var coatColor = get_random(COAT_COLORS);
+  //var eyesColor = get_random(EYES_COLORS);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
   similarListElement.appendChild(wizardElement);
 };
